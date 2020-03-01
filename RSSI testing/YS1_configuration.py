c56d63db8b0fc7ffe49f5fabbd0dc46b220de2e4
MartinaVassallo <56412609+MartinaVassallo@users.noreply.github.com>
2020-03-01 10:23:37 +0100

    Update YS1_configuration.py

--- RSSI testing/YS1_configuration.py
+++ RSSI testing/YS1_configuration.py
@@ -8,21 +8,11 @@ d.setFreq(433920000)
 d.setMdmModulation(MOD_MSK)
 d.makePktVLEN(255)
 d.setMdmDRate(25000)
-# d.setMdmChanSpc(200000)
-# d.setEnableMdmFEC(False)
 
 d.setMdmSyncMode(1)
 d.setMdmSyncWord(0xcccc)
 
-d.calculateMdmDeviatn()
-d.calculatePktChanBW()
-
 d.setPower(0xc0)
-
-# TX_dev = 8000  # deviation to use for Tx
-# RX_dev = d.getMdmDeviatn() # deviation to use for Rx
-# RX_dev = 5000
-# print RX_dev
 
 d.setPktAddr(123)  # address
 d.setEnablePktCRC()  # enable CRC
