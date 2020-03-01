19ff75d2126cb9a1c28e56456854f84c29194fe7
MartinaVassallo <56412609+MartinaVassallo@users.noreply.github.com>
2020-03-01 10:25:34 +0100

    Update YS1_configuration.py

--- LRDL/YS1_configuration.py
+++ LRDL/YS1_configuration.py
@@ -4,12 +4,12 @@ from rflib import *
 
 d = RfCat()
 d.setModeIDLE()
-d.setFreq(433920000)
+d.setFreq(433920000)  # Frequency
 d.setMdmModulation(MOD_MSK)  # Modulation
 d.makePktVLEN(255)      # Maximum possible transmission length set
-d.setMdmDRate(25000)
+d.setMdmDRate(25000)  # Data Rate
 
-d.setMdmSyncMode(1)
+d.setMdmSyncMode(1)  # Sync word settings
 d.setMdmSyncWord(0xcccc)
 
 d.calculatePktChanBW()
@@ -17,5 +17,3 @@ d.calculatePktChanBW()
 d.setPower(0xc0) # 0xc0 = 10 mW, 0x60 = 1 mW
 
 d.setEnablePktCRC()  # enable CRC
-
-
